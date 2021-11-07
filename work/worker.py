cdf68c7802d67ef09e0d3c1e6938a0c06c9a5581
saramentr <89805929+saramentr@users.noreply.github.com>
2021-11-07 15:39:23 +0300

    Update worker.py

--- work/worker.py
+++ work/worker.py
@@ -1,6 +1,37 @@
 import os
 import time
+import shutil
+
+pathGenGit = "/tmp/gengit"
+prePathWorkGit = "/tmp/workgit"
+pathToList = os.getcwd()+"/work/listProjects"
+repoToList = "https://github.com/saramentr/ManualCheck.git"
 
 while True:
-  os.system("ls -al")
+  if os.path.exists(pathGenGit):
+    shutil.rmtree(pathGenGit)
+  os.system("git clone "+repoToList+" "+pathGenGit)
+  if not os.path.exists(pathToList):
+    if os.path.exists(prePathWorkGit):
+      shutil.rmtree(prePathWorkGit)
+  else:
+    if not os.path.exists(prePathWorkGit):
+      os.system("mkdir "+prePathWorkGit)
+    listWorkLine = open(pathToList, 'r').readlines()
+#file pathToList format
+# prjkey_:_urlPort_:_prjlgn_:_organ_:_repo
+    for i in listWorkLine:
+      sepListTmp = i.split("_:_")
+      if len(sepListTmp) != 5:
+        print("error parsing line "+i)
+        continue
+      if os.path.exists(prePathWorkGit+"/"+sepListTmp[3]+"_"+sepListTmp[4]):
+        continue
+      os.system("git clone https://github.com/"+sepListTmp[3]+"/"+sepListTMp[4]+".git "+prePathWorkGit+"/"+sepListTmp[3]+"_"+sepListTmp[4])
+      if not os.path.exists(prePathWorkGit+"/"+sepListTmp[3]+"_"+sepListTmp[4]):
+        print("error clone repo https://github.com/"+sepListTmp[3]+"/"+sepListTMp[4]+".git ")
+        continue
+      os.system("sonar-scanner -Dsonar.projectKey="+sepListTmp[0]+" -Dsonar.sources="+prePathWorkGit+"/"+sepListTmp[3]+"_"+sepListTmp[4]+" -Dsonar.host.url="+sepListTmp[1] +" -Dsonar.login="+sepListTmp[2])
+# fix time to 1000  
   time.sleep(100)
+  os.system("df -h")
